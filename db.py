0cd4801c3cf18d18a1843b555e390928949e69ed
chenglong.zq <chenglong.zq@gmail.com>
2016-11-13 16:47:15 +0100

    surround remove db

--- db.py
+++ db.py
@@ -6,7 +6,10 @@ python db.py
 """
 with sqlite3.connect(database="sample.db") as connection:
     cursor = connection.cursor()
-    cursor.execute("""DROP TABLE IF EXISTS posts""")
+    try:
+        cursor.execute("""DROP TABLE IF EXISTS posts""")
+    except sqlite3.OperationalError:
+        pass
     cursor.execute("""CREATE TABLE posts(title TEXT, description TEXT)""")
     cursor.execute('INSERT INTO posts VALUES("Good", "Good person")')
     cursor.execute('INSERT INTO posts VALUES("Bad", "Bad person")')
